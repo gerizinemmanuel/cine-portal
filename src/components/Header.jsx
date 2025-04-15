e53b6559b43c7fef5bf1fba1b90c43d29bb66517
gerizinemmanuel <emmanuel.gerizim@gmail.com>
2025-04-15 19:08:15 +0200

    use react media query to switch menu type

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
   const [menuMobWidthClass, setMenuMobWidthClass] = useState(
-    " w-0 overflow-hidden "
+    "w-0 overflow-hidden"
   );
   const menuDeskClass = "menu-desk h-full flex items-center gap-5";
-  const menuMobClass = `menu-mob bg-gray-600${menuMobWidthClass}h-[100vh] transition-all duration-[0.2s]  items-end flex flex-col flex-wrap gap-10 absolute z-9999 right-0 top-0`;
+  const menuMobClass = `menu-mob bg-gray-600 ${menuMobWidthClass} h-[100vh] transition-all duration-[0.2s]  items-end flex flex-col flex-wrap gap-10 absolute z-9999 right-0 top-0`;
+
+  const isMobile = useMediaQuery({ maxWidth: 815 });
+
   return (
     <header className="bg-gray-900 h-25 py-0 px-10 flex flex-wrap justify-center gap-[20%] relative">
       <div className="box-logo flex items-center">
@@ -18,15 +22,24 @@ const Header = () => {
           </h1>
         </Link>
       </div>
-      <button
-        id="btn-open-menu-mob"
-        className="hidden"
-        onClick={() => setMenuMobWidthClass(" w-[90%] py-10 px-3 ")}
-      >
-        <Bars3Icon className="size-8" />
-      </button>
+      {isMobile && (
+        <button
+          id="btn-open-menu-mob"
+          onClick={() => setMenuMobWidthClass("w-[90%] py-10 px-3")}
+        >
+          <Bars3Icon className="size-8" />
+        </button>
+      )}
 
-      <nav className={menuDeskClass}>
+      <nav className={isMobile ? menuMobClass : menuDeskClass}>
+        {isMobile && (
+          <button
+            className="absolute left-1 top-1"
+            onClick={() => setMenuMobWidthClass("w-0 overflow-hidden")}
+          >
+            <XMarkIcon className="size-8" />
+          </button>
+        )}
         <li>
           <Link to="/">Início</Link>
         </li>
@@ -39,7 +52,7 @@ const Header = () => {
       </nav>
       {/*Menu Desktop */}
 
-      <nav className={menuMobClass}>
+      {/* <nav className={menuMobClass}>
         <button
           className="absolute left-1 top-1"
           onClick={() => setMenuMobWidthClass(" w-0 overflow-hidden ")}
@@ -55,7 +68,7 @@ const Header = () => {
         <li>
           <Link to="/">Séries</Link>
         </li>
-      </nav>
+      </nav> */}
       {/*Menu Mobile */}
     </header>
   );
